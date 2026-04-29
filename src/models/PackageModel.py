95eb3e16e141ffed98a5ea3737f8baa156d5d3a8
saliha <112631317+salihameryemalbayrak@users.noreply.github.com>
2026-04-29 13:51:16 +0300

    chore: image inputs

--- src/models/PackageModel.py
+++ src/models/PackageModel.py
@@ -3,6 +3,15 @@ from pydantic import Field, validator
 from typing import List, Optional, Union, Literal
 from sdks.novavision.src.base.model import Package, Image, Inputs, Configs, Outputs, Response, Detection, Request, Output, Input, Config
 
+class ReferenceImage(Input):
+    name: Literal["referenceImage"] = "referenceImage"
+    value: Union[List[Image], Image]
+    type: str = "object"
+
+class TestImage(Input):
+    name: Literal["testImage"] = "testImage"
+    value: Union[List[Image], Image]
+    type: str = "object"
 
 class InputReferenceDetections(Input):
     name: Literal["inputReferenceDetections"] = "inputReferenceDetections"
@@ -54,6 +63,8 @@ class IouWeight(Config):
 class PlanogramInputs(Inputs):
     inputReferenceDetections: InputReferenceDetections
     inputTestDetections: InputTestDetections
+    testImage: TestImage
+    referenceImage: ReferenceImage
 
 
 class PlanogramConfigs(Configs):
