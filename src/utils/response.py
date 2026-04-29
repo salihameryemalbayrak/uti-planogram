40e45c6514ab995ad1c9affc69d9ded61d872f12
saliha <112631317+salihameryemalbayrak@users.noreply.github.com>
2026-04-29 13:35:07 +0300

    fix: output format

--- src/utils/response.py
+++ src/utils/response.py
@@ -1,11 +1,11 @@
 
 from sdks.novavision.src.helper.package import PackageHelper
-from components.Package.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, PlanogramOutputs, PlanogramResponse, PlanogramExecutor, OutputData
+from components.Planogram.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, PlanogramOutputs, PlanogramResponse, PlanogramExecutor, OutputData
 
 
 def build_response(context):
-    outputData = OutputData(value=context.image)
-    outputs = PlanogramOutputs(outputImage=outputData)
+    outputData = OutputData(value=context.data)
+    outputs = PlanogramOutputs(outputData=outputData)
     planogramResponse = PlanogramResponse(outputs=outputs)
     planogramExecutor = PlanogramExecutor(value=planogramResponse)
     executor = ConfigExecutor(value=planogramExecutor)
