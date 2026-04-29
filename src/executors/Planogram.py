5e48a9b31735d502049603a98961baf107aeb69f
saliha <112631317+salihameryemalbayrak@users.noreply.github.com>
2026-04-29 11:58:52 +0300

    style: rename executor

--- src/executors/Planogram.py
+++ src/executors/Planogram.py
@@ -15,7 +15,7 @@ from components.Package.src.utils.response import build_response
 from components.Package.src.models.PackageModel import PackageModel
 
 
-class Package(Component):
+class Planogram(Component):
     def __init__(self, request, bootstrap):
         super().__init__(request, bootstrap)
         self.request.model = PackageModel(**(self.request.data))
